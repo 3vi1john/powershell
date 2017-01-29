eec58551c005a2200e10f397a93eb0faf420888f
3vi1john <johndbabio@gmail.com>
2017-01-28 22:58:25 -0500

    opens word docs and searches for SS number
    opens word docs and searches for SS number

---
+++ wordoc-ss.ps1
@@ -0,0 +1,11 @@
+﻿
+$SearchText = "[0-9]{3}[-| ][0-9]{2}[-| ][0-9]{4}"
+$wd = New-Object -ComObject Word.Application
+$wd.Visible = $false
+
+foreach ( $file in $filesearch ) {
+    $doc = $wd.Documents.Open("$file")
+    $doc.Paragraphs | ForEach-Object {
+    Write-Host $file : $_.Range.Text | Where-Object { $_ -match $SearchText } 
+  }
+  $wd.Quit();
